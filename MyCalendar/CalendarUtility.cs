4b3b4a2d9c559bf4ec09115dd4b1447ac9e284e7
Jef Sayloon <jsayloon@iripple.com>
2024-05-25 22:39:36 +0800

    Add project

---
+++ MyCalendar/CalendarUtility.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalendar
+{
+    public static class CalendarUtility
+    {
+        public static string GetMonthName(int curMonth)
+        {
+            string[] monthDictionary = { 
+                "January", "February", "March", "April", "May", "June",
+                 "July", "August", "September", "October", "November", "December"
+            };
+
+            return monthDictionary[curMonth];
+        }
+
+        public static int GetEquivalentNumberOfWeek(string week)
+        {
+            int weekNumber = 1;
+            switch (week)
+            {
+                case var _ when week.StartsWith("Su"): weekNumber = 1; break;
+                case var _ when week.StartsWith("Mo"): weekNumber = 2; break;
+                case var _ when week.StartsWith("Tu"): weekNumber = 3; break;
+                case var _ when week.StartsWith("We"): weekNumber = 4; break;
+                case var _ when week.StartsWith("Th"): weekNumber = 5; break;
+                case var _ when week.StartsWith("Fr"): weekNumber = 6; break;
+                case var _ when week.StartsWith("Sa"): weekNumber = 7; break;
+            }
+
+            return weekNumber;
+        }
+
+        public static string CalendarWeekHeaderFormat()
+        {
+            string weekHeader = string.Format(@"Su  Mo  Tu  We  Th  Fr  Sa");
+
+            return weekHeader;
+        }
+
+        public static void FormatDisplayCalendar(string day, bool isWeekend = false, bool nextLine = false)
+        {
+            const int length = 2;
+            const int blankLength = 2;
+
+            string formattedNumber = day.Length > length ? day.Substring(0, length) : day.PadRight(length);
+            string formattedBlank = "".PadRight(blankLength);
+
+            if (isWeekend) Console.ForegroundColor = ConsoleColor.Green;
+
+            if (!nextLine)
+                Console.Write($"{formattedNumber}{formattedBlank}");
+            else
+                Console.Write($"\n{formattedNumber}{formattedBlank}");
+
+            if (isWeekend) Console.ResetColor();
+        }
+    }
+}
---
+++ MyCalendar/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Text;
+using MyCalendar;
+
+i_run_again:
+Console.Write("Enter year: ");
+if (int.TryParse(Console.ReadLine(), out int year))
+{
+    // validate year
+    // Get Months
+    // Get Days
+    Calendar calendar = CultureInfo.InvariantCulture.Calendar;
+    int months = calendar.GetMonthsInYear(year);
+    int numberOfWeeks = 7;
+
+    Console.WriteLine($"\nCalendar for Year {year}");
+
+    for (int i = 0; i < months; i++)
+    {
+        string monthName = CalendarUtility.GetMonthName(curMonth: i);
+        int days = calendar.GetDaysInMonth(year, month: i + 1);
+
+        Console.Write($"\n{monthName} {year}\n");
+
+        int ctr = 0;
+        for (int j = 0; j < days; j++)
+        {
+            DateTime currDate = new DateTime(year, month: i + 1, day: j + 1);
+            int numberOfWeek = CalendarUtility.GetEquivalentNumberOfWeek(calendar.GetDayOfWeek(currDate).ToString());
+
+            if (ctr == 0)
+            {
+                Console.Write($"{CalendarUtility.CalendarWeekHeaderFormat()} {Environment.NewLine}");
+
+                for (int k = 1; k < numberOfWeeks; k++)
+                {
+                    if (k != numberOfWeek)
+                    { 
+                        CalendarUtility.FormatDisplayCalendar("");
+                        ctr++;
+                    }
+                    else break;
+                }
+            }
+
+            if (ctr < 7)
+            {
+                CalendarUtility.FormatDisplayCalendar((j + 1).ToString(), (numberOfWeek == 1 || numberOfWeek == 7));
+                ctr++;
+            }
+            else
+            { 
+                CalendarUtility.FormatDisplayCalendar((j + 1).ToString(), (numberOfWeek == 1 || numberOfWeek == 7), nextLine: true);
+                ctr = 1;
+            }
+        }
+
+        Console.WriteLine();
+    }
+
+    ask_again:
+    Console.Write("\nDo you want to generate a calander again? [y/n]: ");
+    string answer = Console.ReadLine();
+
+    if (!string.IsNullOrWhiteSpace(answer) && !int.TryParse(answer, out int e) && (answer.StartsWith("y") || answer.StartsWith("n")))
+        if (answer.StartsWith("y"))
+        {
+            Console.Clear();
+            goto i_run_again;
+        }
+        else Environment.Exit(0);
+    else
+    {
+        Console.WriteLine("Invalid");
+        goto ask_again;
+    }
+} 
+else
+{ 
+    Console.WriteLine("Invalid number format");
+}
+
+Console.ReadKey();
